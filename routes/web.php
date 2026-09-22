0f4109374094981bc2d1807f2069fd61cd748221
unknown <you@example.com>
2026-09-22 06:51:49 +0530

    move the /alumni login into a controller

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
+use App\Http\Controllers\AlumniController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/alumni', function () {
-    return view('alumni');
-});
+Route::get('/alumni', [AlumniController::class, 'index'])
+    ->name('alumni.index');
 
 
